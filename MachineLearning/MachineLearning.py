9b194f8e71f7d3ce6f4c281bb70a02578ca4a37f
Payuphat Boontaokaew <125558332+PayuphatBoontaokaew@users.noreply.github.com>
2023-07-22 11:39:05 +0700

    update

--- MachineLearning/MachineLearning.py
+++ MachineLearning/MachineLearning.py
@@ -1,5 +1,4 @@
 from mlforkids import MLforKidsImageProject
-
 # treat this key like a password and keep it secret!
 key = "b0cabba0-2838-11ee-815a-63593f9586039c4a78d5-e47f-472b-8067-6c18a16fcce5"
 
@@ -9,9 +8,8 @@ myproject.train_model()
 
 # CHANGE THIS to the image file you want to recognize
 demo = myproject.prediction("my-test-image.jpg")
-
 label = demo["class_name"]
 confidence = demo["confidence"]
-
 # CHANGE THIS to do something different with the result
 print ("result: '%s' with %d%% confidence" % (label, confidence))
+
