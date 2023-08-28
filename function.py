2e72a82170591d09b554a5d7fb7a55bcd666b53c
Payuphat Boontaokaew <125558332+PayuphatBoontaokaew@users.noreply.github.com>
2023-08-28 15:40:15 +0700

    gogogoggogoggoog

---
+++ function.py
@@ -0,0 +1,20 @@
+# def say():
+#     print('hello')
+
+# say()
+
+# print('Q')
+
+num1 = int(input('num1 : '))
+num2 = int(input('num2 : '))
+
+def e ():
+    r = num1 + num2
+    print('Your num :', r)
+e()
+
+
+
+
+
+    
---
+++ pass.py
@@ -0,0 +1,4 @@
+n = 9
+if n > 10:
+    pass
+print('hello')
