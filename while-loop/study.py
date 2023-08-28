3a1b679976faf100bf9ddff99b408cd000115b0c
Payuphat Boontaokaew <125558332+PayuphatBoontaokaew@users.noreply.github.com>
2023-08-28 15:07:24 +0700

    gogo

--- while-loop/study.py
+++ while-loop/study.py
@@ -1,4 +1,59 @@
-num = 6
-while(num < 9) :
-    print('num :', num)
-    num += 1
+# num = 6
+# while(num < 9) :
+#     print('num :', num)
+#     num += 1
+
+
+# user_input = int(input('Enter integer :'))
+# while (user_input)
+# user_input = int(input('Enter integer :'))
+
+# while True :
+#     user_input = int(input('Enter integer :'))
+#     if user_input == 0 :
+#         print('หยุด')
+#     break
+# print('even num')
+
+
+# while True:
+#     num = int(input("ป้อนจำนวนเต็ม (หากต้องการหยุดให้ป้อน 0): "))
+#     if num == 0:
+#         break
+
+# total = 0
+
+# while total <= 1000:
+#     num = int(input("ป้อนจำนวนเต็ม: "))
+#     total * num
+
+# print("ผลรวมของจำนวนเต็มทั้งหมด:", total)
+
+# num = int(input("ป้อนจำนวนเต็ม : "))
+# result = num
+
+# while result <= 1000:
+#     print("ผลลัพธ์:", result)
+#     result = result * 2
+
+# count = 0
+
+# while True:
+#     num = int(input("ป้อนจำนวนเต็ม : "))
+    
+#     if num == 0:
+#         break
+    
+#     count += 1
+
+# print("ผู้ใช้ป้อนจำนวนครั้งทั้งหมด:", count)
+
+n = int(input('เลข : '))
+while ( n > 0 ) :
+    print('*' , end = '' )
+    n -= 1
+    
+    # x = 10
+    # while (x > 0):
+    #     print("A")
+    #     x -= 1
---
+++ while-loop/study1.py
@@ -0,0 +1,22 @@
+# while True :
+#     user_input = int(input('Enter integer :'))
+#     if user_input % 2 != 0:
+#         print('odd num')
+#     break
+# print('even num')
+
+# i = int(input('Enter integer :'))
+
+# num = int(input("Enter an integer: "))
+# for i in range(num):
+#     print("hello")
+
+n = int(input())
+while n > 0 :
+    print('hello')
+    n -= 1
+
+
+
+
+
