2da3fb93daac3ea554bcc8c0bcb21085dcf54479
Payuphat Boontaokaew <125558332+PayuphatBoontaokaew@users.noreply.github.com>
2023-08-28 15:46:22 +0700

    go

--- function.py
+++ function.py
@@ -5,15 +5,22 @@
 
 # print('Q')
 
-num1 = int(input('num1 : '))
-num2 = int(input('num2 : '))
+# num1 = int(input('num1 : '))
+# num2 = int(input('num2 : '))
 
-def e ():
-    r = num1 + num2
-    print('Your num :', r)
-e()
+# def e ():
+#     r = num1 + num2
+#     print('Your num :', r)
+# e()
 
+# def find_s (num):
+#     r=num*num 
+#     return r
 
+# s = find_s(3)
+# print('s :',s)
+
+print('hello')
 
 
 
